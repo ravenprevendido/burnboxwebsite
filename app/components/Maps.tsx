6a9c6d851d673ada7606ad7f5bb37714eada144a
Raven Prevendido <prevendidoraven_bsit@plmun.edu.ph>
2025-08-28 15:20:27 +0800

    burnbox update

--- app/components/Maps.tsx
+++ app/components/Maps.tsx
@@ -3,12 +3,15 @@ import StoreLocation from './StoreLocation'
 import StoreDetail from './StoreDetail'
 
 const Maps = () => {
+
+  const handleDirectionClick = () => {
+    console.log('Direction clicked!')
+  }
+
   return (
     <div className='min-h-[100vh] min-w-[100vw] z-0 relative pt-20 bg-white'>
         <div className='rounded-lg p-4 h-auto w-full sm:w-[300px] md:w-[350px] lg:w-[400px] absolute top-1/2 -translate-y-1/2 left-5 z-[999] bg-black'>
-          <StoreDetail onDirectionClick={function (): void {
-          throw new Error('Function not implemented.')
-        } }/>
+          <StoreDetail onDirectionClick={handleDirectionClick}/>
         </div>
         <StoreLocation />
     </div>
